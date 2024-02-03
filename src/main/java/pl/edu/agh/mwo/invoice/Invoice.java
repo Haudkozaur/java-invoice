6eb77d71ba20c816eddcd1546ace542d2b773117
student <student@1234>
2024-02-03 11:32:37 +0100

    push push

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -2,22 +2,28 @@ package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
-
+    private Collection<Product> products = new HashSet<>();
+    private Map<String, Integer> productsMap = new HashMap<>();
     public void addProduct(Product product) {
-        // TODO: implement
+        productsMap.put(product.getName(), 1);
+        products.add(product);
     }
-
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        productsMap.put(product.getName(), quantity);
     }
-
     public BigDecimal getSubtotal() {
-        return null;
+        BigDecimal subTotal = new BigDecimal(0);
+        for (Product product : products) {
+            subTotal.add((new BigDecimal(productsMap.get(product.getName()))).multiply(product.getPrice()));
+        }
+        return subTotal;
     }
 
     public BigDecimal getTax() {
@@ -27,4 +33,6 @@ public class Invoice {
     public BigDecimal getTotal() {
         return null;
     }
+
+
 }
