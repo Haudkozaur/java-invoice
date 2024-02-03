9e7522763f255409e84a99ad4e92ff5222ada303
student <student@1234>
2024-02-03 10:20:01 +0100

    essa

--- src/main/java/pl/edu/agh/mwo/invoice/product/TaxFreeProduct.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/TaxFreeProduct.java
@@ -5,5 +5,8 @@ import java.math.BigDecimal;
 public class TaxFreeProduct extends Product {
     public TaxFreeProduct(String name, BigDecimal price) {
         super(name, price, BigDecimal.ZERO);
+        BigDecimal big0 = new BigDecimal(0);
+        if (name == null || name.isEmpty() || price.compareTo(big0) < 0){
+            throw new IllegalArgumentException("Parameters can't be null");}
     }
 }
