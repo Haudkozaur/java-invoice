87289b2f2fecb6d3db3a7e44d9d891a63e31a83e
Haudkozaur <haudek@student.agh.edu.pl>
2024-02-28 02:21:47 +0100

    finished work (maybe not the most elegant, but it is late)

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,52 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.util.*;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
-
+    private Collection<Product> products = new ArrayList<>();
+    private Map<String, Integer> productsMap = new HashMap<>();
     public void addProduct(Product product) {
-        // TODO: implement
+        if (product == null) {
+            throw new IllegalArgumentException("Parameters can't be null");
+        }
+        productsMap.put(product.getName(), 1);
+        products.add(product);
     }
-
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        if (quantity == null || quantity <= 0 || product == null) {
+            throw new IllegalArgumentException("Parameters can't be null");
+        }
+        productsMap.put(product.getName(), quantity);
+        products.add(product);
     }
-
     public BigDecimal getSubtotal() {
-        return null;
+        BigDecimal subTotal = new BigDecimal(0);
+        for (Product product : products) {
+            subTotal = subTotal.add((new BigDecimal(productsMap.get(product.getName()))).multiply(product.getPrice()));
+        }
+        return subTotal;
     }
 
     public BigDecimal getTax() {
-        return null;
+        BigDecimal tax = new BigDecimal(0);
+        for (Product product : products) {
+            tax = tax.add((new BigDecimal(productsMap.get(product.getName()))).multiply(product.getPriceWithTax().subtract(product.getPrice())));
+        }
+        return tax;
+
     }
 
     public BigDecimal getTotal() {
-        return null;
+        BigDecimal total = new BigDecimal(0);
+        for (Product product : products) {
+            total = total.add((new BigDecimal(productsMap.get(product.getName()))).multiply(product.getPriceWithTax()));
+        }
+        return total;
     }
 }
+
+
+
